f7c244b6679f0e12dbbbf783ae5b053d6c9825b5
Firda <firdaarf@gmail.com>
2023-05-26 23:25:21 +0800

    second commit styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="d-flex flex-column align-items-center my-5 App">
       <h1 className="my-2">
-        What's up for today?
+        What's the plan for today?
       </h1>
       <div className="my-3">
         <Todos />
